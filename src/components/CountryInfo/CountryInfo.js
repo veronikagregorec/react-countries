7a6f6cb946dc8c2a8aefb69d909e87a281c58b47
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-23 18:34:09 +0100

    Added Material UI tags and some changes in getCountryByName function

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
 
 function CountryInfo() {
   const [country, setCountry] = useState([]);
@@ -9,67 +10,68 @@ function CountryInfo() {
 
   const { id } = useParams();
 
+  const getCountryByName = async () => {
+    try {
+      const res = await fetch(`https://restcountries.com/v3.1/name/${id}`);
+
+      if (!res.ok) throw new Error("Could not found!");
+
+      const data = await res.json();
+
+      setCountry(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
   useEffect(() => {
-    const getCountryByName = async () => {
-      try {
-        const res = await fetch(`https://restcountries.com/v3.1/name/${id}`);
-
-        if (!res.ok) throw new Error("Could not found!");
-
-        const data = await res.json();
-
-        setCountry(data);
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
-        setError(error.message);
-      }
-    };
-
     getCountryByName();
-  }, [id]);
+  }, []);
 
   return (
-    <div className="wrap-country">
-      {isLoading && !error && <h4 className="loader"></h4>}
+    <Box className="wrap-country">
+      {isLoading && !error && <Box className="loader"></Box>}
       {error && !isLoading && { error }}
 
-      {country?.map((country, index) => (
-        <div className="country-container" key={index}>
-          <div className="row">
-            <div className="country-img">
+      {country.map((country) => (
+        <Box className="country-container" key={country.toString()}>
+          <Box className="row">
+            <Box className="country-img">
               <img src={country.flags.png} alt="" />
-            </div>
+            </Box>
 
             <button>
               <Link to="/">
-                <span>Back to list</span>
+                <Typography variant="span">Back to list</Typography>
               </Link>
             </button>
-          </div>
+          </Box>
 
-          <div className="country-info">
-            <h3>{country.name.common}</h3>
+          <Box className="country-info">
+            <Typography variant="h3">{country.name.common}</Typography>
 
-            <div className="country-info-details">
-              <h5>
+            <Box className="country-info-details">
+              <Typography variant="h5">
                 Region: <span>{country.region}</span>
-              </h5>
-              <h5>
+              </Typography>
+              <Typography variant="h5">
                 Sub Region: <span>{country.subregion}</span>
-              </h5>
-              <h5>
+              </Typography>
+              <Typography variant="h5">
                 Capital: <span>{country.capital}</span>
-              </h5>
-              <h5>
+              </Typography>
+              <Typography variant="h5">
                 Country Code: {country.idd.root}
                 {country.idd.suffixes}
-              </h5>
-            </div>
-          </div>
-        </div>
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 }
 
