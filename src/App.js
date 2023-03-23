e7eca23846ae5062f5369731a626c6d2343443a2
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-23 18:28:00 +0100

    Added Material UI tags

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AllCountries from "./components/AllCountries/AllCountries";
 import CountryInfo from "./components/CountryInfo/CountryInfo";
+import { Typography, Box } from "@mui/material";
 
 function App() {
   return (
     <>
-      <div className="header">
-        <div className="container">
-          <h2>Countries</h2>
-        </div>
-      </div>
-      <div className="container">
+      <Box className="header">
+        <Box className="container">
+          <Typography variant="h2">Countries</Typography>
+        </Box>
+      </Box>
+      <Box className="container">
         <Routes>
           <Route path="/" element={<AllCountries />} />
           <Route path="/country/:id" element={<CountryInfo />} />
         </Routes>
-      </div>
+      </Box>
     </>
   );
 }
